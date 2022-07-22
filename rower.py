23668b2667dcc4275a8cfd72ed10d564c0dc4045
testandwin <mschlottmann74@gmail.com>
2022-07-22 18:11:30 +0200

    Finetuning // requirements.txt added

--- rower.py
+++ rower.py
@@ -7,7 +7,7 @@ import numpy as np
 
 FACTOR_DISTANCE = 0.15
 """The factor to multiply the distance with to get the appr. distance of a stroke"""
-MIN_STROKE_DISTANCE = 3
+MIN_STROKE_DISTANCE = 2
 """Used to prevent unlogic distance changes"""
 NANO_TO_SECOND = 1000000000
 
@@ -74,6 +74,9 @@ class Rower:
         """Handling the backward direction change. Just some values are stored"""
         self.start_backward_time = time_in_ns
         self.distance = (distance - self.start_forward_distance) * FACTOR_DISTANCE
+        # In case something weird happens, we don't want to have a negative distance
+        if self.distance < 0:
+            self.distance = 0
 
     def calculate_data(self, t_forward, t_backward):
         """Caluclate the different metrics based on the numbers and return an JSON object with the values"""
@@ -102,6 +105,7 @@ class Rower:
             'average_500m': sec_to_min(total_time / self.total_distance * 500),
             'running': self.running
         }
+        print("Distance:", self.distance)
         return d
 
 def sec_to_min(sec):
